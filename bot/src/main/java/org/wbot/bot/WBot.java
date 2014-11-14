77946ba7b198c28c8626792cdaa5227953e56822
Johan <johan.f.ljungberg@gmail.com>
2014-11-14 13:57:12 +0100

    Basics added

---
+++ bot/src/main/java/org/wbot/bot/WBot.java
@@ -0,0 +1,9 @@
+package org.wbot.bot;
+
+/**
+ * Created by Johan on 2014-11-14.
+ */
+public class WBot {
+
+
+}
---
+++ bot/src/main/java/org/wbot/bot/script/ScriptManager.java
@@ -0,0 +1,16 @@
+package org.wbot.bot.script;
+
+import org.jruby.embed.ScriptingContainer;
+
+/**
+ * Created by Johan on 2014-11-14.
+ */
+public class ScriptManager {
+
+    private ScriptingContainer container;
+
+    public ScriptManager() {
+
+    }
+
+}
